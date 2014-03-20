66f3c9064c04cb6324113ea5eb995ef3d857c076
Przemysław Zając <zajac@rtechnologies.pl>
2014-03-20 10:47:02 +0100

    debug

--- lib/bazarka_allegro/integration/product.rb
+++ lib/bazarka_allegro/integration/product.rb
@@ -76,7 +76,7 @@ module BazarkaAllegro
             extension_for_product.details.each do |k,v|
           hash.merge!(k => v)
         end
-       hash.merge( 'attribute_1' => self.name )
+       hash.merge!( 'attribute_1' => self.name )
 
         return hash
 
