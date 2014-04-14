6e91e87ce3d1d883b2fe9a6ed7fa6443a2e17a2b
Przemysław Zając <zajac@rtechnologies.pl>
2014-04-14 12:33:34 +0200

    modyfikacja klasy journal

--- lib/bazarka_allegro/hooks/journal.rb
+++ lib/bazarka_allegro/hooks/journal.rb
@@ -9,26 +9,30 @@ module BazarkaAllegro
 
       # Pobieranie informacji z dziennika zdarzeń nt. zdarzeń dot. ofert.
       def do_get_site_journal(starting_point = nil, info_type = 0)
-          message = {session_handle: @client.session_handle, starting_point: starting_point, info_type: info_type }
+          message = {session_handle: @client.session_handle, info_type: info_type }
+          message.merge!(starting_point: starting_point) unless starting_point.nil?
           @client.call(:do_get_site_journal, message: message)
       end
 
       # Pobranie informacji z dziennika zdarzeń nt. zdarzeń dot. formularzy pozakupowych.
       def do_get_site_journal_deals(starting_point = nil)
-        message = {session_id: @client.session_handle, journal_start: starting_point }
+        message = {session_id: @client.session_handle }
+        message.merge!(starting_point: starting_point) unless starting_point.nil?
         @client.call(:do_get_site_journal_deals, message: message)
       end
 
       # Pobieranie informacji z dziennika zdarzeń o liczbie zmian nt. zdarzeń dot. formularzy pozakupowych.
       def do_get_site_journal_deals_info(starting_point = nil)
-        message = {session_id: @client.session_handle, journal_start: starting_point }
+        message = {session_id: @client.session_handle }
+        message.merge!(starting_point: starting_point) unless starting_point.nil?
         @client.call(:do_get_site_journal_deals_info, message: message)
 
       end
 
       # Pobieranie informacji z dziennika zdarzeń o liczbie zmian nt. zdarzeń dot. ofert.
       def do_get_site_journal_info(starting_point = nil, info_type = 0)
-        message = {session_handle: @client.session_handle, starting_point: starting_point, info_type: info_type }
+        message = {session_handle: @client.session_handle, info_type: info_type }
+        message.merge!(starting_point: starting_point) unless starting_point.nil?
         @client.call(:do_get_site_journal_info, message: message)
       end
 
