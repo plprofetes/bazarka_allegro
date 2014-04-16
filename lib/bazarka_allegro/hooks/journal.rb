74e999d122491493dd60c8bb1247eb8b888f32a2
Przemysław Zając <zajac@rtechnologies.pl>
2014-04-16 12:53:47 +0200

    poprwka nazwy parametrów wysyłanych w metodach klasy Journal

--- lib/bazarka_allegro/hooks/journal.rb
+++ lib/bazarka_allegro/hooks/journal.rb
@@ -17,21 +17,21 @@ module BazarkaAllegro
       # Pobranie informacji z dziennika zdarzeń nt. zdarzeń dot. formularzy pozakupowych.
       def do_get_site_journal_deals(starting_point = nil)
         message = {session_id: @client.session_handle }
-        message.merge!(starting_point: starting_point) unless starting_point.nil?
+        message.merge!(journal_start: starting_point) unless starting_point.nil?
         @client.call(:do_get_site_journal_deals, message: message)
       end
 
       # Pobieranie informacji z dziennika zdarzeń o liczbie zmian nt. zdarzeń dot. formularzy pozakupowych.
       def do_get_site_journal_deals_info(starting_point = nil)
         message = {session_id: @client.session_handle }
-        message.merge!(starting_point: starting_point) unless starting_point.nil?
+        message.merge!(journal_start: starting_point) unless starting_point.nil?
         @client.call(:do_get_site_journal_deals_info, message: message)
 
       end
 
       # Pobieranie informacji z dziennika zdarzeń o liczbie zmian nt. zdarzeń dot. ofert.
       def do_get_site_journal_info(starting_point = nil, info_type = 0)
-        messd4age = {session_handle: @client.session_handle, info_type: info_type }
+        message = {session_handle: @client.session_handle, info_type: info_type }
         message.merge!(starting_point: starting_point) unless starting_point.nil?
         @client.call(:do_get_site_journal_info, message: message)
       end
