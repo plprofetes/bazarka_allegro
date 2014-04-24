16a1cf80a9c089a69a16b5da44e178a840fdd9b3
Przemysław Zając <zajac@rtechnologies.pl>
2014-04-24 14:12:19 +0200

    loggr

--- lib/bazarka_allegro/product_allegro.rb
+++ lib/bazarka_allegro/product_allegro.rb
@@ -143,6 +143,9 @@ module BazarkaAllegro
     def update_item_quantity(product)
       if product.quantity.to_i > 0
         begin
+        Rails.logger.info "----------------"
+        Rails.logger.info product.quantity.to_i
+        Rails.logger.info "----------------"
         response = @allegro.do_change_quantity_item(product.extension_for_products.where(key: 'allegro').first.allegro_id, product.quantity.to_i)
         rescue Savon::SOAPFault => e
           Rails.logger.info "#{e}"
