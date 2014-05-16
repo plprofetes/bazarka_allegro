c99aca23b91ad270f49ec900e474505bb7b0c275
Przemysław Zając <zajac@rtechnologies.pl>
2014-05-16 11:27:34 +0200

    dodanie jeszcze jednego ifa sprawdzającego czy produkt jest na allegro

--- lib/bazarka_allegro/product_allegro.rb
+++ lib/bazarka_allegro/product_allegro.rb
@@ -142,6 +142,7 @@ module BazarkaAllegro
     # aktualizacja ilości, jeśli ilość mniejsza od zera ustawiamy produkt niedostępny
     def update_item_quantity(product)
       extension_for_product = product.extension_for_products.where(key: 'allegro').where(published: true).first
+      if extension_for_product.present?
       if extension_for_product.allegro_quantity.to_i > 0
         begin
           # pobieramy id aukcji allegro, pobieramy dane aukcji, ile jest produktów i ile było na początku
@@ -190,6 +191,7 @@ module BazarkaAllegro
       end
       #Rails.logger.info response.inspect
       response
+      end
     end
 
 
