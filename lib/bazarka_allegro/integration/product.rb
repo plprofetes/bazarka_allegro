fafbf0f9d360733672faaa364c8c8b565d1ef4cc
Przemysław Zając <zajac@rtechnologies.pl>
2014-05-30 09:41:06 +0200

    zamiana description na description_allegro

--- lib/bazarka_allegro/integration/product.rb
+++ lib/bazarka_allegro/integration/product.rb
@@ -85,7 +85,7 @@ module BazarkaAllegro
         extension_for_product.details.each do |k,v|
           hash.merge!(k => v)
         end
-        hash.merge!( 'attribute_1' => self.name, 'attribute_24' => self.description,
+        hash.merge!( 'attribute_1' => self.name, 'attribute_24' => self.description_allegro,
                       'attribute_8' => self.price )
 
         # jeśli śledzimy ilość w bazarce to bierzemy liczbę z produktu
